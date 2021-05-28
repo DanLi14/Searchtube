1effb00d0e97b0c019f93e35a6686326ddb15d3e
Man Chung Daniel Li <danli@Mans-MacBook-Pro-2.local>
2021-05-28 18:02:24 +0100

    Implement a SearchBar from semantic ui
    Format SearchBar in SearchBar component and add a header tag.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,28 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  state = { term: '' };
 
-    render() {
-        return <div>SearchBar</div>
-    }
+  render() {
+    return (
+      <div className="ui segment">
+        <h1>SearchTube</h1>
+        <form className="ui search">
+          <div className="ui icon input">
+            <input
+              className="prompt"
+              type="text"
+              placeholder="Search..."
+              value={this.state.term}
+              onChange={this.onInput}
+            ></input>
+            <i className="search icon"></i>
+          </div>
+          <div className="results"></div>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default SearchBar
+export default SearchBar;
