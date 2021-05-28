e37cdb3c9a07a3aa39f0676474fe46ac0712d671
Man Chung Daniel Li <danli@Mans-MacBook-Pro-2.local>
2021-05-28 18:12:53 +0100

    Add state to SearchBar
    Value of SearchBar now changes based on user input which is registered
    as a state {term} property in the SearchBar component.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,11 +3,20 @@ import React from 'react';
 class SearchBar extends React.Component {
   state = { term: '' };
 
+  onInput = (e) => {
+    this.setState({ term: e.target.value });
+  };
+
+  onFormSubmit = (e) => {
+    e.preventDefault();
+    // Callback prop from parent component required.
+  };
+
   render() {
     return (
       <div className="ui segment">
         <h1>SearchTube</h1>
-        <form className="ui search">
+        <form className="ui search" onSubmit={this.onFormSubmit}>
           <div className="ui icon input">
             <input
               className="prompt"
