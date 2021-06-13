a0bd7e34d3008ea989cc90ecb0cc91af3fd5cc5c
Man Chung Daniel Li <danli@Mans-MacBook-Pro-2.local>
2021-06-13 17:01:29 +0100

    update .env file so that youtube.js file can be found by Heroku.

---
+++ src/api/youtube.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const {REACT_APP_YOUTUBE_API_KEY} = process.env 
+
+
+// const KEY = 'AIzaSyAhx0FOXoYFuLHdFgbdHL_xRDIBGezlOss';
+const KEY = `${REACT_APP_YOUTUBE_API_KEY}`;
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: KEY,
+    type: 'video',
+  },
+});
+
